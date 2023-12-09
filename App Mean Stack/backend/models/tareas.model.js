a66aa09134607e07e8e22486b6f522acd825ff2a
Mateo Javier Condor Sosa <96481656+MateoCondor@users.noreply.github.com>
2023-12-08 22:28:23 -0500

    Delete App Mean Stack directory

--- App Mean Stack/backend/models/tareas.model.js
+++
@@ -1,9 +0,0 @@
-const {model, Schema} = require('mongoose');
-
-const tareaSchema = new Schema({
-    title: String,
-    desc: String,
-    complete: Boolean
-});
-
-module.exports = model('tarea', tareaSchema);
--- App Mean Stack/backend/routes/api.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.use('/tareas', require('./api/tareas'));
-
-module.exports = router;
